ed2dde88dca5eda2f6ca53bf9c74e0f8df7628d6
sesaworks <support@sesaworks.com>
2022-05-04 22:46:29 +0530

    Update Game.js

--- src/js/Game.js
+++ src/js/Game.js
@@ -21,7 +21,7 @@ var gameOptions = {
     pipeDistance: [150, 280],
  
     // hole range between pipes, in pixels
-    pipeHole: [180, 260],
+    pipeHole: [200, 260],
  
     // local storage object name
     localStorageName: 'bestFlappyScore'
